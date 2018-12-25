e0c9ff39bd49b051f37f86e00e539272ac48750b
navjotpatti19 <pattinavjot19@gmail.com>
2018-12-24 17:04:47 -0800

    Log and Toast Changes

--- app/src/main/java/com/example/sapple/googlemapsdirection/MainActivity.kt
+++ app/src/main/java/com/example/sapple/googlemapsdirection/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.sapple.googlemapsdirection
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+        Log.e("Main Activity", "Navjot")
+        Toast.makeText(this, "Hola", Toast.LENGTH_LONG).show()
     }
 }
